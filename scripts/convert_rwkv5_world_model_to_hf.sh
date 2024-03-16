f1d506aeb2e406e557f739ec88dc926dbef1fa2b
BBuf <1182563586@qq.com>
2024-03-16 16:42:58 +0800

    refine

--- scripts/convert_rwkv5_world_model_to_hf.sh
+++ scripts/convert_rwkv5_world_model_to_hf.sh
@@ -2,12 +2,12 @@
 set -x
 
 cd scripts
-# python convert_rwkv5_checkpoint_to_hf.py --repo_id BlinkDL/rwkv-5-world \
-#  --checkpoint_file RWKV-5-World-3B-v2-20231118-ctx16k.pth \
-#  --output_dir ../../rwkv5-v2-world-3b/ \
-#  --tokenizer_file ../rwkv5_model \
-#  --size 3B \
-#  --is_world_tokenizer True
+python convert_rwkv5_checkpoint_to_hf.py --repo_id BlinkDL/rwkv-5-world \
+ --checkpoint_file RWKV-5-World-3B-v2-20231118-ctx16k.pth \
+ --output_dir ../../rwkv5-v2-world-3b/ \
+ --tokenizer_file ../rwkv5_model \
+ --size 3B \
+ --is_world_tokenizer True
 
 cp ../rwkv5_model/vocab.txt ../../rwkv5-v2-world-3b/
 cp ../rwkv5_model/tokenization_rwkv5.py ../../rwkv5-v2-world-3b/
