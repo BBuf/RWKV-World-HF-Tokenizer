ecd3ea00c63933756d5ffc109d614dc7d4a0aece
SmerkyG <SmerkyG@users.noreply.github.com>
2024-03-19 17:00:52 -0700

    more conversion to new style for v5

--- rwkv_world_v5_model_batch/modeling_rwkv5.py
+++ rwkv_world_v5_model_batch/modeling_rwkv5.py
@@ -192,31 +192,27 @@ class Rwkv5LinearAttention(torch.autograd.Function):
             return (None, None, None, None, greceptance, g_key, g_value, g_time_decay, g_time_first)
 
 
-def rwkv5_linear_attention_cpu(
-    receptance,
-    key,
-    value,
-    time_decay,
-    time_first,
-    state,
-):
-    input_dtype = key.dtype
-    B,T,C = receptance.shape
-    H, S = time_first.shape
-    key = key.to(torch.float32).view(B, T, H, S).transpose(1, 2).transpose(-2, -1)
-    value = value.to(torch.float32).view(B, T, H, S).transpose(1, 2)
-    receptance = receptance.to(torch.float32).view(B, T, H, S).transpose(1, 2)
-    time_decay = torch.exp(-torch.exp(time_decay.float())).reshape(-1, 1, 1).reshape(H, -1, 1)
-    time_first = time_first.float().reshape(-1, 1, 1).reshape(H, -1, 1)
-    out = torch.zeros_like(key).reshape(B, T, H, S)
-    for t in range(T):
-        rt = receptance[:, :, t : t + 1, :]
-        kt = key[:, :, :, t : t + 1]
-        vt = value[:, :, t : t + 1, :]
-        at = kt @ vt
-        out[:, t] = (rt @ (time_first * at + state)).squeeze(2)
+def rwkv5_linear_attention_cpu(receptance, key, value, time_decay, time_first, state):
+    input_dtype = receptance.dtype
+    # For CPU fallback. Will be slower and probably take more memory than the custom CUDA kernel if not executed
+    # within a torch.no_grad.
+    batch, seq_length, num_heads = receptance.shape
+    num_heads, head_size = time_first.shape
+    key = key.float().view(batch, seq_length, num_heads, head_size).transpose(1, 2).transpose(-2, -1)
+    value = value.float().view(batch, seq_length, num_heads, head_size).transpose(1, 2)
+    receptance = receptance.float().view(batch, seq_length, num_heads, head_size).transpose(1, 2)
+    time_decay = torch.exp(-torch.exp(time_decay.float())).reshape(-1, 1, 1).reshape(num_heads, -1, 1)
+    time_first = time_first.float().reshape(-1, 1, 1).reshape(num_heads, -1, 1)
+    out = torch.zeros_like(key).reshape(batch, seq_length, num_heads, head_size)
+
+    for current_index in range(seq_length):
+        current_receptance = receptance[:, :, current_index:current_index+1, :]
+        current_key = key[:, :, :, current_index:current_index+1]
+        current_value = value[:, :, current_index:current_index+1, :]
+        attention_output = current_key @ current_value
+        out[:, current_index] = (current_receptance @ (time_first * attention_output + state)).squeeze(2)
         with torch.no_grad():
-            state = at + time_decay * state
+            state = attention_output + time_decay * state
 
     return out.to(input_dtype), state
 
@@ -239,14 +235,10 @@ class Rwkv5SelfAttention(nn.Module):
         super().__init__()
         self.config = config
         kernel_loaded = rwkv5_cuda_kernel is not None and rwkv5_cuda_kernel.head_size == config.head_size
-        print("Maybe Loading CUDA")
         if is_ninja_available() and is_torch_cuda_available() and not kernel_loaded:
-            print("Loading CUDA")
             try:
                 load_wkv5_cuda_kernel(config.head_size)
             except Exception:
-                print ("Could not load the custom CUDA kernel for RWKV6 attention.")
-                raise
                 logger.info("Could not load the custom CUDA kernel for RWKV5 attention.")
         self.layer_id = layer_id
         hidden_size = config.hidden_size
